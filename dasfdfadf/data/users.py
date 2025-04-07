520e9a74c334ab00b946c30bb2049579faa9cfc9
matveizzzzzz <owduozx@gmail.com>
2025-04-07 17:09:40 +0300

    Add files via upload

---
+++ dasfdfadf/data/users.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)

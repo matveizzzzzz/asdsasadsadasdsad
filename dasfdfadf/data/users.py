ad66f8d0075bf055cad3e66529e3f73c6d63a2ea
matveizzzzzz <owduozx@gmail.com>
2025-04-07 17:10:29 +0300

    Update users.py

--- dasfdfadf/data/users.py
+++ dasfdfadf/data/users.py
@@ -5,6 +5,9 @@ from .db_session import SqlAlchemyBase
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)

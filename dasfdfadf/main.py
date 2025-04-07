b4331c94062b94994ba3f8394e3f53482bbf0fca
matveizzzzzz <owduozx@gmail.com>
2025-04-07 17:11:50 +0300

    Update main.py

--- dasfdfadf/main.py
+++ dasfdfadf/main.py
@@ -9,18 +9,52 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/blogs.db")
     user = User()
-    user.surname = 'Scott'
-    user.name = 'Ridley'
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = 'research engineer'
-    user.address = 'module_1'
-    user.email = 'scott_chief@mars.org'
+    user.surname = "Scott"
+    user.name = "Ridley"
+    user.age = "21"
+    user.position = "captain"
+    user.speciality = "research enginee"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
     db_sess = db_session.create_session()
     db_sess.add(user)
     db_sess.commit()
     app.run()
 
+
+def main():
+    db_session.global_init("db/blogs.db")
+    user = User()
+    user.surname = 'Nacist'
+    user.name = 'Egor'
+    user.age = 15
+    user.position = 'student'
+    user.speciality = 'autistic'
+    user.address = 'bebelya 109'
+    user.email = 'egor@mars.org'
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+    app.run()
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    user = User()
+    user.surname = 'Tatar'
+    user.name = 'Samat'
+    user.age = 16
+    user.position = 'student'
+    user.speciality = 'chillguy'
+    user.address = 'hz 100'
+    user.email = 'samat@mars.org'
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+    app.run()
 
 if __name__ == '__main__':
     main()
